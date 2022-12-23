f328a36483e567187af9599820e4b3031d68387c
Maulen <azikulov9@gmail.com>
2022-12-23 14:36:50 +0600

    первая страница

---
+++ assets/scripts/layout.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".header .header__menu");
+
+  function toggleSideMenu() {
+    document.querySelector(".side-menu").classList.toggle("side-menu_hide");
+    document.querySelector(".container-lg").classList.toggle("side-menu-hide")
+  }
+
+  menuButton.addEventListener("click", toggleSideMenu);
+});
