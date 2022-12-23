3fbd627e3c5f6e3c4e0a24647e0e01c39f0742e2
Maulen <azikulov9@gmail.com>
2022-12-23 21:22:46 +0600

    адаптация выписки

--- assets/scripts/layout.js
+++ assets/scripts/layout.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.querySelector(".header .header__menu");
+  const sideMenu = document.querySelector(".side-menu");
+  const containerLarge = document.querySelector(".container-lg");
 
   function toggleSideMenu() {
-    document.querySelector(".side-menu").classList.toggle("side-menu_hide");
-    document.querySelector(".container-lg").classList.toggle("side-menu-hide");
+    sideMenu.classList.toggle("side-menu_hide");
+    containerLarge.classList.toggle("side-menu-hide");
   }
 
   menuButton.addEventListener("click", toggleSideMenu);
+
+  // Mobile functions
+  const closeSideMenuButton = document.querySelector(
+    ".side-menu .side-menu__close"
+  );
+
+  function closeSideMenu() {
+    if (window.screen.width <= 768) {
+      sideMenu.classList.add("side-menu_hide")
+    }
+  }
+
+  closeSideMenuButton.addEventListener("click", closeSideMenu);
 });
